c59d7fee6b9ab2649fa5eaf5b768b279fe8763bf
ShubhamKashyap1001 <shubh9142996613@gmail.com>
2025-07-31 11:48:52 +0530

    add LeaderboardPage component

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,12 +1,10 @@
 'use client'
 import { Signin } from "@/components/Signin/Signin";
-import Leaderboard from "@/components/Leaderboard";
 
 export default function SigninPage (){
     return(
         <div>
             <Signin />
-            <Leaderboard />
         </div>
     )
 }

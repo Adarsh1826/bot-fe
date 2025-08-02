56b84775a91d9e02d2a253a2480766b59e60013d
ShubhamKashyap1001 <shubh9142996613@gmail.com>
2025-08-03 00:57:40 +0530

    Add contributionGrid

---
+++ app/Grid/page.jsx
@@ -0,0 +1,11 @@
+'use client';
+
+import ContributionGrid from '@/components/boards/ContributionGrid';
+
+export default function Page() {
+  return (
+    <main className="flex justify-center bg-black min-h-screen">
+      <ContributionGrid />
+    </main>
+  );
+}
